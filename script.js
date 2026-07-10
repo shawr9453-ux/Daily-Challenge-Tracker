5e2d554fef8373e211e46b56c55fe36211e48cab
shawr9453-ux <shawr9453@gmail.com>
2026-07-10 20:58:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+
+
   const startBtn = document.querySelector("button");
+
   const welcome = document.getElementById("welcome");
 
+
+
   startBtn.addEventListener("click", function () {
+
     const username = document.getElementById("username").value.trim();
 
+
+
     if (username === "") {
-      alert("Username daalo");
+
+      alert("Enter Username");
+
       return;
+
     }
 
+
+
     localStorage.setItem("daily_user", username);
+
     welcome.innerText = "Welcome, " + username;
+
   });
+
+
 
 });
